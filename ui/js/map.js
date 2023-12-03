775fa959fbddd9185377035f5680554203386a0d
TAKAHASHI Tetsuro <tetsuta@mit.edu>
2023-12-03 11:45:32 +0900

    add control to get info

--- ui/js/map.js
+++ ui/js/map.js
@@ -1,21 +1,57 @@
 var Evacquide = function() {
     function main() {
-	// setupButtons();
+	var on_shit;
+	var on_control;
+	on_shift = false;
+	on_control = false;
+
+
+	setupControlls();
 	var map = L.map('map')
-	map.setView([35.40, 136], 9);
 
 	L.tileLayer('https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png', {
             attribution: "<a href='https://maps.gsi.go.jp/development/ichiran.html' target='_blank'>国土地理院</a>"
 	}).addTo(map);
 
+	map.setView([36.948, 140.903], 15);
 	// open street map
 	// L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	//     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 	// }).addTo(map);	
 
+
+	map.on('click', function(e) {
+	    // shift-clickで座標を表示する
+	    if (on_shift == true) {
+		lat = e.latlng.lat;
+		lng = e.latlng.lng;
+		alert("lat: " + lat + ", lng: " + lng);
+	    }
+	});
+
+	map.on('move', function(e) {
+	});
     }
 
-    function setupButtons() {
+    function setupControlls() {
+	$(window).keyup(function(e) {
+	    if (e.key == "Control") {
+		on_control = false;
+	    }
+	    if (e.key == "Shift") {
+		on_shift = false;
+	    }
+	});
+
+	$(window).keydown(function(e) {
+	    if (e.key == "Control") {
+		on_control = true;
+	    }
+	    if (e.key == "Shift") {
+		on_shift = true;
+	    }
+	});
+
     }
 
     return {
