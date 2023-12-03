a5a9377db285f482379226d318020983a58221ef
TAKAHASHI Tetsuro <tetsuta@mit.edu>
2023-12-03 13:58:55 +0900

    click to put cross mark

--- ui/js/map.js
+++ ui/js/map.js
@@ -1,5 +1,6 @@
 var Evacquide = function() {
     var map;
+    var crossIcon;
 
     function main() {
 	var on_shit;
@@ -19,7 +20,13 @@ var Evacquide = function() {
 	// open street map
 	// L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	//     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-	// }).addTo(map);	
+	// }).addTo(map);
+
+	crossIcon = L.icon({
+	    iconUrl: 'image/cross-sign.png',
+	    iconSize:     [30, 30], // size of the icon
+	    iconAnchor:   [15, 15], // point of the icon which will correspond to marker's location
+	});
 
 
 	map.on('click', function(e) {
@@ -29,18 +36,36 @@ var Evacquide = function() {
 	    // shift-clickで座標を表示する
 	    if (on_shift == true) {
 		alert("lat: " + lat + ", lng: " + lng);
-		return;
-	    } 
-
-	    // clickでマーカーを載せる
-	    var report_detail = "timestamp<br><img src='https://cdn.mainichi.jp/vol1/2022/11/29/20221129k0000m040094000p/9.jpg?1' width='500' height='375'>";
-	    var popup1 = L.popup({ maxWidth: 550 }).setContent(report_detail);
-	    var marker = L.marker([lat, lng]).bindPopup(popup1).bindTooltip("report on timestamp").addTo(map);
-
+	    } else {
+		// clickでバツを表示する
+		// 再度クリックしたら消す
+		L.marker([lat, lng], {icon: crossIcon}).on('click', onCrossClick).addTo(map);
+	    }
 	});
+
 
 	map.on('move', function(e) {
 	});
+
+	report(36.94891755154147, 140.90274810791018);
+	report(36.94812872265479, 140.90515136718753);
+	report(36.947511372610805, 140.90772628784183);
+
+    }
+
+    function onCrossClick(e){
+	map.removeLayer(e.target);
+    }
+
+    function report(lat, lng){
+	var report_detail = "timestamp<br><img src='https://cdn.mainichi.jp/vol1/2022/11/29/20221129k0000m040094000p/9.jpg?1' width='500' height='375'>";
+	var popup = L.popup({ maxWidth: 550 }).setContent(report_detail);
+	var marker = L.marker([lat, lng]).bindPopup(popup).bindTooltip("report on timestamp").addTo(map);
+    }
+
+
+    function mon(text){
+	$('#monitor').text(text);
     }
 
     function setupControlls() {
@@ -56,9 +81,11 @@ var Evacquide = function() {
 	$(window).keydown(function(e) {
 	    if (e.key == "Control") {
 		on_control = true;
+		mon("control");
 	    }
 	    if (e.key == "Shift") {
 		on_shift = true;
+		mon("shift");
 	    }
 	});
 
