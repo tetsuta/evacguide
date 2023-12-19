a99cfb0f9ad5205a3d31bbad187624e0cde291db
TAKAHASHI Tetsuro <tetsuta@mit.edu>
2023-12-19 21:28:13 +0900

    mobile.html can get parameters, lat and lon

--- ui/js/mobile.js
+++ ui/js/mobile.js
@@ -26,10 +26,31 @@ var Evacquide = function() {
 	//     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 	// }).addTo(map);
 
-	map.setView([33.5808303, 130.340], 18);
+
+	var param = location.search;
+	var lat_str = getParam('lat');
+	var lon_str = getParam('lon');
+	if (lat_str == null || lon_str == null) {
+	    map.setView([33.5808303, 130.340], 18);
+	} else {
+	    // console.log(lat_str);
+	    // console.log(lon_str);
+	    map.setView([33.5808303, Number(lon_str)], 18);
+	}
 
 	// 最初にすべてを読み込む
 	updateAllInfo();
+    }
+
+
+    function getParam(name, url) {
+	if (!url) url = window.location.href;
+	name = name.replace(/[\[\]]/g, "\\$&");
+	var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+            results = regex.exec(url);
+	if (!results) return null;
+	if (!results[2]) return '';
+	return decodeURIComponent(results[2].replace(/\+/g, " "));
     }
 
 
@@ -53,13 +74,11 @@ var Evacquide = function() {
 	} else {
 	    var time_num = Date.parse(anreport.table);
 	    if ((now_num - time_num) < threshold_millisec) {
-		// mon("o:" + anreport.table)
 		var report_detail = anreport.table + "<br><a href='" + anreport.URL + "' target='_blank'><img src='" + anreport.URL + "' width='300' height='600'></a>";
 		var popup = L.popup({ maxWidth: 330, maxHeight: 660 }).setContent(report_detail);
 		var tooltip_text = "report on " + anreport.table;
 		var marker = L.marker([Number(anreport.lat), Number(anreport.lon)]).bindPopup(popup).bindTooltip(tooltip_text).addTo(map);
 	    } else {
-		// mon("x:" + anreport.table)
 	    }
 	    marker_set[anreport.table] = marker;
 	}
