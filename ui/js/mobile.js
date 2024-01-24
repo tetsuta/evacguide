4a154902c971b5aa9bd0f0f165dd97e9f028fa6a
TAKAHASHI Tetsuro <tetsuta@mit.edu>
2024-01-24 19:56:05 +0900

    mobile.js shows only the user's traces given by sid

--- ui/js/mobile.js
+++ ui/js/mobile.js
@@ -2,14 +2,25 @@ var Evacquide = function() {
     var now = new Date();
     var now_num = now.getTime();
     var threshold_millisec = 1000 * 60 * 60 * 24 * 365;
+
+    // この時刻以降の traceを表示する
+    var show_trace_start_time = "2023/12/13 13:26:45";
     var map = null;
     var current_location = null;
+
+    var humanIcon;
+    var humanGrayIcon;
+    var traceIcon;
+    var traceGrayIcon;
 
     // 描画した markerを記録する
     var marker_set = {};
 
     var current_lat = null;
     var current_lng = null;
+
+    // 対象となるユーザの sessionID
+    var sid = null;
 
     var pulsingIcon = L.icon.pulse({
 	iconSize:[20,20],
@@ -19,6 +30,9 @@ var Evacquide = function() {
     });
 
     var first_location_found = true;
+
+    var shown_history_set = {}; // shown_history_set[sid][time] = icon_marker
+    var trace_history;
 
     function main() {
 	// ------------------------------
@@ -83,7 +97,8 @@ var Evacquide = function() {
 
 	// ------------------------------
 	map = L.map('map', {
-	    layers: [maplist[0], overlaylist[0]]  // default layer
+	    // layers: [maplist[0], overlaylist[0]]  // default layer
+	    layers: [maplist[1]]  // default layer
 	    // trackResize: true,
 	});
 	map.zoomControl.setPosition('bottomright');
@@ -128,9 +143,9 @@ var Evacquide = function() {
 	var param = location.search;
 	var lat_str = getParam('lat');
 	var lon_str = getParam('lon');
+	sid = getParam('sid');
 	if (lat_str == null || lon_str == null) {
 	    console.log("default location");
-
 	    map.setView([33.5808303, 130.340], 18);
 	} else {
 	    console.log("gived location");
@@ -151,11 +166,8 @@ var Evacquide = function() {
 	// 最初にすべてを読み込む
 	updateAllInfo();
 
+	// for test あとでコメントを外す。そうしないと現在地に行かない
 	map.locate({setView: true, maxZoom: 16});
-	// console.log("go3");
-	// console.log("go4");
-	// console.log("go5");
-	// console.log("go6");
 
 
 	// initial legend
@@ -164,8 +176,123 @@ var Evacquide = function() {
 	$('#hz2').html(overlayLabel_wo_legend['hz2']);
 	$('#hz3').html(overlayLabel_wo_legend['hz3']);
 
+
+	// ------------------------------
+	humanIcon = L.icon({
+	    iconUrl: 'image/MapNoHito2.png',
+	    iconSize:     [24, 50], // size of the icon
+	    iconAnchor:   [12, 25], // point of the icon which will correspond to marker's location
+	});
+
+	humanGrayIcon = L.icon({
+	    iconUrl: 'image/MapNoHito2_gray.png',
+	    iconSize:     [24, 50], // size of the icon
+	    iconAnchor:   [12, 25], // point of the icon which will correspond to marker's location
+	});
+
+	traceIcon = L.icon({
+	    iconUrl: 'image/trace.png',
+	    iconSize:     [10, 10], // size of the icon
+	    iconAnchor:   [5, 5], // point of the icon which will correspond to marker's location
+	});
+
+	traceGrayIcon = L.icon({
+	    iconUrl: 'image/trace_gray.png',
+	    iconSize:     [10, 10], // size of the icon
+	    iconAnchor:   [5, 5], // point of the icon which will correspond to marker's location
+	});
+
+
+	// ------------------------------
+	show_trace_history();
 	auto_update_current_location();
     }
+
+
+    function show_trace_history(){
+	if (sid != null) {
+	    getAllTraces(show_trace_start_time);
+
+	    if (trace_history[sid].length > 0) {
+		var stime = trace_history[sid][0].stime;
+		while (stime * 1000 < now_num) {
+		    ahistory = trace_history[sid].shift();
+		    put_history(sid, ahistory);
+		    if (trace_history[sid].length == 0) {
+			break;
+		    } else {
+			stime = trace_history[sid][0].stime;
+		    }
+		}
+	    }
+	    update_history_icon(now_num);
+	}
+    }
+
+    function put_history(asid, history){
+	var tooltip_text = asid + "<br>updated at " + history.time;
+	var history_mark = L.marker([Number(history.lat), Number(history.lon)], {
+	    icon: humanIcon,
+	    zIndexOffset: 1000
+	}).bindTooltip(tooltip_text).addTo(map);
+
+	if (!(asid in shown_history_set)) {
+	    shown_history_set[asid] = {}
+	}
+	shown_history_set[asid][history.stime] = history_mark;
+    }
+
+
+    function update_history_icon(playback_time_msec){
+	for (let sid in shown_history_set) {
+	    var max_time = 0;
+	    var recent_time_list = [];
+	    var old_time_list = [];
+	    for (let time in shown_history_set[sid]) {
+		if (max_time < time) {
+		    max_time = time;
+		}
+		if (playback_time_msec > (time * 1000 + 60 * 1000)) {
+		    old_time_list.push(time);
+		} else {
+		    recent_time_list.push(time);
+		}
+	    }
+	    // console.log(sid + ":" + max_time);
+	    if (playback_time_msec > (max_time * 1000 + 60 * 1000)) {
+		shown_history_set[sid][max_time].setZIndexOffset(20);
+		shown_history_set[sid][max_time].setIcon(humanGrayIcon)
+	    }
+	    old_time_list.forEach(a_time => {
+		if (a_time != max_time) {
+		    shown_history_set[sid][a_time].setZIndexOffset(10);
+		    shown_history_set[sid][a_time].setIcon(traceGrayIcon)
+		}
+	    })
+	    recent_time_list.forEach(a_time => {
+		if (a_time != max_time) {
+		    shown_history_set[sid][a_time].setZIndexOffset(500);
+		    shown_history_set[sid][a_time].setIcon(traceIcon)
+		}
+	    })
+	}
+    }
+
+
+    function getAllTraces(time){
+        $.ajax({
+            type: 'POST',
+            url: new Config().getUrl() + '/',
+            async: false,
+            data: JSON.stringify({
+                mode: "getAllTraces",
+		time: time
+	    }),
+        }).done(function(data) {
+	    trace_history = data.trace_history;
+        });
+    }
+
 
     function auto_update_current_location() {
 	var update_current_location = function() {
